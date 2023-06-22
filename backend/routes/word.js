73036c4073f73d6ea889fcffb23974598cb9a151
Yuya Minamide <dettu1003@gmail.com>
2023-06-22 14:12:01 -0700

    added delete word controller

--- backend/routes/word.js
+++ backend/routes/word.js
@@ -3,6 +3,7 @@ import { editWordController } from "../controllers/editWordController.js";
 import { getWordController } from "../controllers/getWordController.js";
 import { archiveWordController } from "../controllers/archiveWordController.js";
 import { removeWordController } from "../controllers/removeWordController.js";
+import { deleteWordController } from "../controllers/deleteWordController.js";
 //import authentication from "../middleware/auth.js";
 
 import { Router } from "express";
@@ -14,3 +15,4 @@ router.post("/addword", addWordController);
 router.put("/editword/:id", editWordController);
 router.put("/archiveword/:id", archiveWordController);
 router.put("/removeword/:id", removeWordController);
+router.delete("/deleteword/:id", deleteWordController);
