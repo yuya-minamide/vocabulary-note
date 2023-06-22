8882ec5f10c0ca1979487fb28322271f16aa49b1
Yuya Minamide <dettu1003@gmail.com>
2023-06-21 21:32:03 -0700

    added function of getting word from DB

--- frontend/src/components/pages/Home/HomeContents.jsx
+++ frontend/src/components/pages/Home/HomeContents.jsx
@@ -1,10 +1,30 @@
 import { HomeAddWord, HomeWordList } from "../../index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineUpload } from "react-icons/ai";
+import { useSelector } from "react-redux";
 import { AddButton, HomeContainer } from "../../../styles/components/pages/Home/HomeContentsStyle";
 
 export function HomeContents() {
+	const userData = useSelector((state) => state.user);
+	const userId = userData._id;
 	const [showAddWord, setShowAddWord] = useState(false);
+	//const [isLoading, setIsLoading] = useState(true);
+	const [words, setWords] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/getword`);
+			const resData = await response.json();
+
+			if (userId) {
+				const userWords = resData.filter((word) => word.userid === userId);
+				setWords(userWords);
+			} else {
+				setWords([]);
+			}
+		};
+		fetchData();
+	}, [userId]);
 
 	const handleAddButtonClick = () => {
 		setShowAddWord(true);
@@ -26,7 +46,7 @@ export function HomeContents() {
 				<AiOutlineUpload onClick={handleAddButtonClick} />
 			</AddButton>
 			{showAddWord && <HomeAddWord onClose={handleCloseClick} onSave={handleSaveClick} />}
-			<HomeWordList />
+			<HomeWordList words={words} />
 		</HomeContainer>
 	);
 }
--- frontend/src/components/pages/Home/HomeWordList.jsx
+++ frontend/src/components/pages/Home/HomeWordList.jsx
@@ -1,13 +1,17 @@
 import { ListButton, WordListContainer } from "../../../styles/components/pages/Home/HomeWordListStyle";
 
-export function HomeWordList() {
+export function HomeWordList({ words }) {
 	return (
-		<WordListContainer>
-			<div>Apple</div>
-			<div>
-				<ListButton>Edit</ListButton>
-				<ListButton>Achieve</ListButton>
-			</div>
-		</WordListContainer>
+		<>
+			{words.map((word) => (
+				<WordListContainer key={word._id}>
+					<div>{word.egword}</div>
+					<div>
+						<ListButton>Edit</ListButton>
+						<ListButton>Achieve</ListButton>
+					</div>
+				</WordListContainer>
+			))}
+		</>
 	);
 }
