35604252b30ff95053e0e87601baa7e1d69c31a8
Yuya Minamide <dettu1003@gmail.com>
2023-06-28 20:03:49 -0700

    added a count down before start exam

--- frontend/src/pages/Exam/Exam.jsx
+++ frontend/src/pages/Exam/Exam.jsx
@@ -1,13 +1,37 @@
 import { ExamContents, Footer } from "../../components/index";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { CountContainer, Countdown } from "../../styles/pages/Exam/ExamStyle";
 
 export function Exam() {
 	const location = useLocation();
 	const examWord = location.state?.examWord;
+	const [countdown, setCountdown] = useState(3); // State to store the countdown value
+
+	useEffect(() => {
+		// Timer to decrement the countdown value every second
+		const timer = setInterval(() => {
+			setCountdown((prevCountdown) => prevCountdown - 1);
+		}, 1000);
+
+		// Cleanup the timer when the component unmounts or countdown reaches 0
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
 	return (
 		<div>
-			<ExamContents examWord={examWord} />
-			<Footer />
+			{countdown > 0 ? (
+				<CountContainer>
+					<Countdown>{countdown}</Countdown>
+				</CountContainer> // Render the countdown value
+			) : (
+				<>
+					<ExamContents examWord={examWord} />
+					<Footer />
+				</>
+			)}
 		</div>
 	);
 }
